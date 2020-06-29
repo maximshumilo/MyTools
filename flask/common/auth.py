6146a73c07f8bf5bca5d121dedbc191d4e200b62
Shumilo Maxim <shumilo.mk@gmail.com>
2020-06-29 18:25:27 +0400

    Update auth module

--- flask/common/auth.py
+++ flask/common/auth.py
@@ -1,86 +1,85 @@
-from functools import wraps
 from flask import _request_ctx_stack, has_request_context, session
 
 
-def authenticate(user_model, user=None, email: str = None, password: str = None, token: str = None):
-    """
-    Аутентифицирует пользователя по паре email-password
+class UserSession:
+    User = None
 
-    :param user_model: Модель пользователя
-    :param user: Пользователь
-    :param email: Email пользователя
-    :param password: Пароль пользователя в открытом виде
-    :param token: Токен пользователя
-    :return: Аутентифицированный пользователь в случае совпадения пароля, иначе None
-    """
-    try:
-        if not user:
-            user = user_model.get_by_email(email, status='active')
-        if not user:
-            return None
+    def __init__(self, user_model, type_db):
+        """
 
-        if password:
-            assert user.check_password(password)
-            return user
+        """
+        self.User = user_model
+        self.type_db = type_db
 
-        if token:
-            assert user.check_token(token)
-            return user
-    except AssertionError:
-        pass
-    return None
+    @staticmethod
+    def authenticate(user_instance=None, email: str = None, password: str = None, token: str = None):
+        """
+        Аутентифицирует пользователя по паре email-password
 
+        :param user_instance: Пользователь
+        :param email: Email пользователя
+        :param password: Пароль пользователя в открытом виде
+        :param token: Токен пользователя
+        :return: Аутентифицированный пользователь в случае совпадения пароля, иначе None
+        """
+        try:
+            if not user_instance:
+                user_instance = user_instance.get_by_email(email, status='active')
+            if not user_instance:
+                return None
 
-def login(user, remember: bool = False) -> bool:
-    """
-    Аутентифицирует пользователя в текущей сессии
+            if password:
+                assert user_instance.check_password(password)
+                return user_instance
 
-    :param user: Пользователь, для которого открывается сессия
-    :param remember: Флаг запоминания пользователя после окончания сессии
-    :return: Результат открытия аутентифицированной сессии
-    """
-    if not user.active:
-        return False
+            if token:
+                assert user_instance.check_token(token)
+                return user_instance
+        except AssertionError:
+            pass
+        return None
 
-    user_id = user.id
-    session['user_id'] = str(user_id)
-    if hasattr(_request_ctx_stack.top, 'user'):
-        delattr(_request_ctx_stack.top, 'user')
+    @staticmethod
+    def login(user_instance, remember: bool = False) -> bool:
+        """
+        Аутентифицирует пользователя в текущей сессии
 
-    if remember:
-        session['remember'] = remember
-    return True
+        :param user_instance: Пользователь, для которого открывается сессия
+        :param remember: Флаг запоминания пользователя после окончания сессии
+        :return: Результат открытия аутентифицированной сессии
+        """
+        if not user_instance.active:
+            return False
 
+        user_id = user_instance.id
+        session['user_id'] = str(user_id)
+        if hasattr(_request_ctx_stack.top, 'user'):
+            delattr(_request_ctx_stack.top, 'user')
 
-def logout():
-    """
-    Заканчивает активную пользовательскую сессию
+        if remember:
+            session['remember'] = remember
+        return True
 
-    :return: Результат завершения сессии
-    """
-    session.pop('user_id', None)
+    @staticmethod
+    def logout():
+        """
+        Заканчивает активную пользовательскую сессию
 
-    _request_ctx_stack.top.user = None
-    return True
+        :return: Результат завершения сессии
+        """
+        session.pop('user_id', None)
+        _request_ctx_stack.top.user = None
+        return True
 
+    def current_user(self):
+        if has_request_context() and not hasattr(_request_ctx_stack.top, 'user'):
+            user_id = session.get('user_id')
+            if user_id:
+                user = None
+                if self.type_db == 'nosql':
+                    user = self.User.objects.filter(state='active', id=user_id).first()
+                elif self.type_db == 'sql':
+                    user = self.User.where(state='active', id=user_id).first()
+                _request_ctx_stack.top.user = user
 
-def current_user(user_model):
-    if has_request_context() and not hasattr(_request_ctx_stack.top, 'user'):
-        user_id = session.get('user_id')
-
-        if user_id:
-            user = user_model.where(state='active', id=user_id).first()
-            _request_ctx_stack.top.user = user
-
-    return getattr(_request_ctx_stack.top, 'user', None)
-
-
-def login_required(model):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            if not current_user(model):
-                return {'errors': {"auth": 'Not authenticated'}}, 401
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
+        return getattr(_request_ctx_stack.top, 'user', None)
