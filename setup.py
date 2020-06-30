c45b488bcf9ffd5de58cdffb5b6eda2e26ea3794
Shumilo Maxim <shumilo.mk@gmail.com>
2020-06-30 15:52:01 +0400

    Add readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='maximshumilo_tools',
-    version='0.0.5',
+    version='0.1.0',
     packages=['ms_tools', 'ms_tools.flask', 'ms_tools.flask.sql', 'ms_tools.flask.mongo', 'ms_tools.flask.common',
               'ms_tools.common'],
     url='https://t.me/MaximShumilo',
@@ -10,5 +10,5 @@ setup(
     author='Maxim Shumilo',
     author_email='shumilo.mk@gmail.com',
     description='My development tools',
-    install_requires=['bs4', 'mongoengine', 'flask']
+    install_requires=['flask', 'requests', 'marshmallow', 'mongoengine', 'natsort']
 )
