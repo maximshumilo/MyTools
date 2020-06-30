36df94beff640d10af3522b1a6c4bde8fd3992b9
Shumilo Maxim <shumilo.mk@gmail.com>
2020-06-30 12:27:33 +0400

    Edit setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     packages=['ms_tools', 'ms_tools.flask', 'ms_tools.flask.sql', 'ms_tools.flask.mongo', 'ms_tools.flask.common',
               'ms_tools.common'],
-    url='',
+    url='https://t.me/MaximShumilo',
     license='',
     author='Maxim Shumilo',
     author_email='shumilo.mk@gmail.com',
