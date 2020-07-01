58a7e5fd93e0525f17033bc91b2431d2ff69d2db
Shumilo Maxim <shumilo.mk@gmail.com>
2020-07-01 15:18:17 +0400

    Fix bug convert to instance

--- ms_tools/flask/common/marshmallow.py
+++ ms_tools/flask/common/marshmallow.py
@@ -9,6 +9,7 @@ def convert_to_instance(model, type_db, field='id', many=False, allow_deleted=Fa
 
     def get_value_from_instances(instances):
         """Get field in instances"""
+        instances = instances if isinstance(instances, list) else [instances]
         return [getattr(doc, field) for doc in instances]
 
     def query(value, many_instances=False, **kwargs):
@@ -29,15 +30,13 @@ def convert_to_instance(model, type_db, field='id', many=False, allow_deleted=Fa
 
     def convert_one(value, **kwargs):
         """Convert to one instance"""
-        instance = query(value, **kwargs).first()
-        return get_value_from_instances([instance])[0] if kwargs.get('return_field') else instance
+        return query(value, **kwargs).first()
 
     def convert_many(value, **kwargs):
         """Convert to many instances"""
         values = value.split(',')
         values = list(set(values))
-        instances = query(values, many_instances=True, **kwargs).all()
-        return get_value_from_instances(instances) if kwargs.get('return_field') else instances
+        return query(values, many_instances=True, **kwargs).all()
 
     def to_instance(*args, **kwargs):
         """
@@ -50,7 +49,7 @@ def convert_to_instance(model, type_db, field='id', many=False, allow_deleted=Fa
             raise ValidationError('Invalid identifier', field_name=field)
         if not result:
             raise ValidationError(error, field_name=field)
-        return result
+        return get_value_from_instances(result) if kwargs.get('return_field') else result
 
     return partial(to_instance, model=model, type_db=type_db, field=field,
                    many=many, error=error, return_field=return_field)
--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.rst", "r") as fh:
 
 setup(
     name='maximshumilo_tools',
-    version='0.1.2',
+    version='0.1.4',
     packages=['ms_tools', 'ms_tools.flask', 'ms_tools.flask.sql', 'ms_tools.flask.mongo', 'ms_tools.flask.common',
               'ms_tools.common'],
     url='https://t.me/MaximShumilo',
     license='',
     author='Maxim Shumilo',
     author_email='shumilo.mk@gmail.com',
-    install_requires=['flask', 'requests', 'marshmallow', 'mongoengine', 'unittests'],
+    install_requires=['flask', 'requests', 'marshmallow', 'mongoengine'],
     include_package_data=True,
     long_description=long_description,
 
