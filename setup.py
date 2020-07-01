4d312f0c068072879ae68c332d85bfca99815c00
Shumilo Maxim <shumilo.mk@gmail.com>
2020-07-01 15:00:53 +0400

    Add readme to long_description

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup
 
-# read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 
 setup(
     name='maximshumilo_tools',
-    version='0.1.3',
+    version='0.1.2',
     packages=['ms_tools', 'ms_tools.flask', 'ms_tools.flask.sql', 'ms_tools.flask.mongo', 'ms_tools.flask.common',
               'ms_tools.common'],
     url='https://t.me/MaximShumilo',
     license='',
     author='Maxim Shumilo',
     author_email='shumilo.mk@gmail.com',
+    install_requires=['flask', 'requests', 'marshmallow', 'mongoengine', 'unittests'],
+    include_package_data=True,
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    install_requires=['flask', 'requests', 'marshmallow', 'mongoengine', 'unittests']
+
 )
